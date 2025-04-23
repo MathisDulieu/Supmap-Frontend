eb5233015611a260d512044ef79ceb79f7dcb054
Mathis <106063734+MathisDulieu@users.noreply.github.com>
2025-04-23 16:45:39 +0200

    [/] Env config

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -3,14 +3,12 @@
 echo "API_BASE_URL: ${API_BASE_URL}"
 echo "GOOGLE_API_KEY: ${GOOGLE_API_KEY}"
 
-cat > /usr/share/nginx/html/env-config.js << EOF
-window.env = {
-  API_BASE_URL: "${API_BASE_URL}",
-  GOOGLE_API_KEY: "${GOOGLE_API_KEY}"
-};
-EOF
+sed -i 's|<script src="./public/env-config.js"></script>||g' /usr/share/nginx/html/index.html
+sed -i 's|<script src="/env-config.js"></script>||g' /usr/share/nginx/html/index.html
 
-ls -la /usr/share/nginx/html/
-cat /usr/share/nginx/html/env-config.js
+sed -i "s|<head>|<head>\n    <script>window.env = { API_BASE_URL: \"${API_BASE_URL}\", GOOGLE_API_KEY: \"${GOOGLE_API_KEY}\" };</script>|g" /usr/share/nginx/html/index.html
+
+echo "Contenu injecté dans index.html :"
+grep -A 3 "window.env" /usr/share/nginx/html/index.html
 
 exec "$@"
--- env-config.js
+++
@@ -1,4 +0,0 @@
-window.env = {
-    API_BASE_URL: '${API_BASE_URL}',
-    GOOGLE_API_KEY: '${GOOGLE_API_KEY}'
-};
